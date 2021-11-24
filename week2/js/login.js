3c7e1daeb6a3a8d714b7861fd4c1ffec0af1cc31
LeTuanAnh0301 <94361840+LeTuanAnh0301@users.noreply.github.com>
2021-11-24 17:49:51 +0700

    Add files via upload

---
+++ week2/js/login.js
@@ -0,0 +1,17 @@
+function isSuccessLogin() 
+{
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username + " " + password);
+    if (username == "John" && password == "1234")
+        {
+            alert("Đăng nhập thành công");
+            window.location.href = ("http://127.0.0.1:5500/html/dashbroad.html");
+        }
+    else
+        {
+            document.getElementById("demo").innerHTML = "<p>" + "User name or password invalid" + "</p>";
+            document.getElementById("demo").style.color = "red";
+        }
+}
